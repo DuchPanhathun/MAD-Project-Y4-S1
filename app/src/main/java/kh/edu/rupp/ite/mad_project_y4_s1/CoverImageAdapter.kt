667951cd0656304b2345f62fbbc6bb905f2766c9
DuchPanhathun <duch.panhathun.2821@rupp.edu.kh>
2024-09-21 15:29:49 +0700

    first commit

---
+++ app/src/main/java/kh/edu/rupp/ite/mad_project_y4_s1/CoverImageAdapter.kt
@@ -0,0 +1,31 @@
+package kh.edu.rupp.ite.mad_project_y4_s1
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class CoverImageAdapter(private val images: List<Int>) : RecyclerView.Adapter<CoverImageAdapter.CoverImageViewHolder>() {
+    
+    inner class CoverImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val coverImageView: ImageView = itemView.findViewById(R.id.coverImageView)
+
+        fun bind(image: Int) {
+            Log.d("CoverImageAdapter", "Binding image resource: $image")
+            coverImageView.setImageResource(image)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoverImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_cover_image, parent, false)
+        return CoverImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CoverImageViewHolder, position: Int) {
+        holder.bind(images[position])
+    }
+
+    override fun getItemCount(): Int = images.size
+}
--- app/src/main/java/kh/edu/rupp/ite/mad_project_y4_s1/MainActivity.kt
+++ app/src/main/java/kh/edu/rupp/ite/mad_project_y4_s1/MainActivity.kt
@@ -1,20 +1,28 @@
 package kh.edu.rupp.ite.mad_project_y4_s1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Set drawables programmatically
+        findViewById<ImageButton>(R.id.searchButton).setImageResource(R.drawable.ic_search)
+        findViewById<ImageButton>(R.id.shoppingButton).setImageResource(R.drawable.ic_shopping_bag)
+        findViewById<ImageView>(R.id.logoImage).setImageResource(R.drawable.app_logo)
+
+        val coverImageCarousel: ViewPager2 = findViewById(R.id.coverImageCarousel)
+        val images = listOf(
+            R.drawable.cover_image1,
+            R.drawable.cover_image2,
+            R.drawable.cover_image3
+        )
+        
+        coverImageCarousel.adapter = CoverImageAdapter(images)
     }
 }
