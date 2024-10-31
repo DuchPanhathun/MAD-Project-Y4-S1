bee70398d6b28d0d81414bb96e103f4d84463215
DuchPanhathun <duch.panhathun.2821@rupp.edu.kh>
2024-10-31 09:23:43 +0700

    ..

---
+++ app/src/main/java/kh/edu/rupp/ite/mad_project_y4_s1/model/ ApiResponse.kt
@@ -0,0 +1,13 @@
+package kh.edu.rupp.ite.mad_project_y4_s1.model
+
+data class ApiResponse<T>(
+    val state: ApiState,
+    val data: T? = null,
+    val error: String? = null
+)
+
+enum class ApiState {
+    LOADING,
+    SUCCESS,
+    ERROR
+}
